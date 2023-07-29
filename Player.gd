baa377af1d7278c9488e53e3c305e185c0c599fb
randomnewb <nguy1581@gmail.com>
2023-07-29 10:15:33 -0500

    raycast that points in direction player is looking

--- Player.gd
+++ Player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @export var speed = 100;
+@onready var ray_cast_2d = $RayCast2D
 
 func _ready():
 	pass;
@@ -19,6 +20,9 @@ func _process(delta):
 #	position.x = clamp(position.x, 5, width - 5);
 #	position.y = clamp(position.y, 5, height - 5);
 	#position += input_vector * speed * delta;
+	
+	ray_cast_2d.target_position = input_vector * 25;
+	
 	var collision = move_and_collide(input_vector * speed * delta);
 	if collision:
 		print(collision.get_collider().name)
