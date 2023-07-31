78402c67081df8d16bcf3cdd5335580db08dadf5
randomnewb <nguy1581@gmail.com>
2023-07-30 23:25:09 -0500

    sword swing animation on win

--- Player.gd
+++ Player.gd
@@ -7,35 +7,38 @@ var interacting = false;
 var collision = null;
 var CONTROL_PROGRESS_SCENE = preload("res://UI/control_progress.tscn")
 
+@onready var sprite_2d = $Sprite2D
+@onready var animation_player = $AnimationPlayer
+
 signal mini_game_won
 
 func _process(delta):
-	var input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-
-	if Input.is_action_pressed("tap_left_mouse"):
-		last_clicked_position = get_local_mouse_position();
-		var target = last_clicked_position;
-		target /= position;
-		input_vector = target.normalized();
-
-	if input_vector == Vector2.ZERO:
-#		animation_player.play("idle");
-		pass;
-	else:
-#		animation_player.play("run");
-		if input_vector.x != 0:
-			pass;
-#			sprite_2d.scale.x = sign(input_vector.x);
-#	position.x = clamp(position.x, 5, width - 5);
-#	position.y = clamp(position.y, 5, height - 5);
-	#position += input_vector * speed * delta;
-	
-	#ray_cast code
-#	ray_cast_2d.target_position = input_vector * 25;
-#	var ray_collide = ray_cast_2d.get_collider()
-#	if ray_collide:
-#		print("ray: ",ray_collide)
 	if not interacting:
+		var input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+
+		if Input.is_action_pressed("tap_left_mouse"):
+			last_clicked_position = get_local_mouse_position();
+			var target = last_clicked_position;
+			target /= position;
+			input_vector = target.normalized();
+
+		if input_vector == Vector2.ZERO:
+	#		animation_player.play("idle");
+			pass;
+		else:
+	#		animation_player.play("run");
+			if input_vector.x != 0:
+				sprite_2d.scale.x = 0.1 * sign(input_vector.x);
+	#	position.x = clamp(position.x, 5, width - 5);
+	#	position.y = clamp(position.y, 5, height - 5);
+		#position += input_vector * speed * delta;
+		
+		#ray_cast code
+	#	ray_cast_2d.target_position = input_vector * 25;
+	#	var ray_collide = ray_cast_2d.get_collider()
+	#	if ray_collide:
+	#		print("ray: ",ray_collide)
+		
 		collision = move_and_collide(input_vector * speed * delta);
 	
 	if collision and not interacting:
@@ -51,6 +54,9 @@ func _process(delta):
 		control_progress_scene.mini_game_completed.connect(self._on_player_mini_game_completed);
 
 func _on_player_mini_game_completed():
-	interacting = false;
+	animation_player.play("swing");
+	await get_tree().create_timer(1.0).timeout;
 	mini_game_won.emit();
+	interacting = false;
+	animation_player.play("RESET");
 	self.set_global_position(Vector2(20,20));
